7b100400d15b5c7b7064c5311447388115864b2f
Mark Lester Echavez <marklester.echavez@gmail.com>
2024-01-07 10:31:04 +0800

    Add copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,6 +5,11 @@ return {
       -- vim.g.copilot_filetypes = { xml = false }
       -- or
       -- vim.g.copilot_filetypes = { ["*"] = false, python = true }
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      --
+      vim.api.nvim_set_keymap("i", "<A-f>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
 }
