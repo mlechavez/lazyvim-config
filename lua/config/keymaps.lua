f3ef2addec9ee1013c758ef94a15dd4bb26b6734
Mark Lester Echavez <marklester.echavez@gmail.com>
2025-01-03 20:37:37 -0500

    refactor: remap move lines to avoid conflict in glazeWM

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,11 @@
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Go to normal mode" })
 vim.keymap.set("n", "<leader>t", ":ToggleTerm direction=float<CR>", { desc = "Opens the terminal", noremap = true })
+
+-- Move Lines
+vim.keymap.set("n", "<C-A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<C-A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<C-A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<C-A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<C-A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<C-A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
