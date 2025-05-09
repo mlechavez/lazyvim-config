72104f777206a5fb7ac4fb8af139d6464e77c836
Mark Lester Echavez <marklester.echavez@gmail.com>
2025-05-09 13:52:53 -0400

    feat: add terraform

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
