d3c357c01e84ec89b4eb74a6742edf172b5c1ff7
Mark Lester Echavez <marklester.echavez@gmail.com>
2026-06-13 05:32:11 -0400

    feat!: migrate from omnisharp to dotnet language server

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
